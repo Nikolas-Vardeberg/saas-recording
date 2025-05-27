230237683f004e253fe72793a3a785d1ad32703f
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-05-27 18:05:25 +0200

    feat: database setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
     return(
         <main className="wrapper page">
             <Header title="All Videos" subHeader="Public library" />
-            <h1>Welcome to loom clone</h1>
 
             <section className="video-grid">
                 {dummyData.map((card) => (
